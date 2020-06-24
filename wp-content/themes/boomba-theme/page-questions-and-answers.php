05911cc6dd112572b19e327ec0f5113c87086ca0
netanel-sogodigital <netanel@sogo.co.il>
2020-06-24 17:56:14 +0300

    Work on display

--- wp-content/themes/boomba-theme/page-questions-and-answers.php
+++ wp-content/themes/boomba-theme/page-questions-and-answers.php
@@ -11,18 +11,19 @@ the_post();
             <div class="col-md-8 col-xs-12">
                 <div class="row justify-content-center">
                     <div class="col-md-12 col-xs-12  text-center py-3 py-lg-4">
-                        <h2 class="h1 mb-3 h-border-top text-color-5"><?php the_title() ?></h2>
+                        <h2 class="h1 mb-3mb-3 h-border-top text-color-5"><?php the_title() ?></h2>
                         <div class="entry-content ">
                             <?php the_content() ?>
                         </div>
 
-                        <div class="accordion panel-group  rounded" id="accordionExample">
+                        <div class="accordion panel-group  rounded " id="accordionExample_<?php echo $counter; ?>">
                             <?php $counter = 1; ?>
                             <?php while (have_rows('boom_questions-and-answers')): the_row(); ?>
-                                <div class="box-shadow-1 rounded mb-3 text-left">
+                                <div class="rounded mb-3 text-left ">
                                     <div class="card">
-                                        <div class="card-header " id="heading_<?php echo $counter; ?>">
+                                        <div class="card-header" id="heading_<?php echo $counter; ?>">
                                             <h4 class="mb-0">
+                                                <span><i class="fa fa-plus"></i> </span>
                                                 <button class="btn btn-link text-color-5 h4 " type="button" data-toggle="collapse"
                                                         data-target="#collapse_<?php echo $counter; ?>"
                                                         aria-expanded="true"
@@ -30,12 +31,13 @@ the_post();
                                                     <?php echo get_sub_field('question') ?>
                                                 </button>
                                             </h4>
+
                                         </div>
 
                                         <div id="collapse_<?php echo $counter; ?>"
                                              class="collapse_<?php echo $counter; ?> collapse"
                                              aria-labelledby="heading_<?php echo $counter; ?>"
-                                             data-parent="#accordionExample">
+                                             data-parent="#accordionExample_<?php echo $counter; ?>">
                                             <div class="card-body">
                                                 <?php echo get_sub_field('answers') ?>
                                             </div>
@@ -44,10 +46,7 @@ the_post();
                                 </div>
                                 <?php $counter++; ?>
                             <?php endwhile; ?>
-
                         </div>
-
-
                     </div>
                 </div>
             </div>
